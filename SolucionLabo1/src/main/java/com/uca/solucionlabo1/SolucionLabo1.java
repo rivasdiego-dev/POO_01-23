4190d49761b43865bbc4d0f4e2b294d4f4d6d139
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-03-25 14:36:07 -0600

    Warning added to project :rotating_light:

--- SolucionLabo1/src/main/java/com/uca/solucionlabo1/SolucionLabo1.java
+++ SolucionLabo1/src/main/java/com/uca/solucionlabo1/SolucionLabo1.java
@@ -6,7 +6,14 @@ import javax.swing.JOptionPane;
 public class SolucionLabo1 {
 
     public static void main(String[] args) {
-        
+        JOptionPane.showMessageDialog(null, """
+                                            Acabas de intentar correr el proyecto completo
+                                            Para correr cada ejercicio por separado, en el archivo main del ejericio
+                                            que quieras correr, presiona la combinación de teclas:
+                                                        
+                                                                                            SHIFT + F6
+                                            
+                                            """, "Creo que no querías hacer esto...",0);
     }
 }
     
